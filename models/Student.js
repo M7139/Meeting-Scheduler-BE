539bdcdb8a8cd493f55ad330d4bad65a303ca1c4
M7139 <mohamedaljoamee@gmail.com>
2025-08-26 11:02:55 +0300

    basic file setup

--- models/Student.js
+++ models/Student.js
@@ -6,7 +6,6 @@ const studentSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordDigest: { type: String, required: true }
-    // no contact, department, office, availability here
   },
   { timestamps: true }
 )
---
+++ server.js
@@ -0,0 +1,32 @@
+//imports
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+
+
+
+// Initialize app
+const app = express();
+
+// Database Configuration
+const db = require('./db')
+
+// set Port Configuration
+const port = process.env.PORT ? process.env.PORT : 3000
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Your app is connected . . . ')
+})
+
+// Listener
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
